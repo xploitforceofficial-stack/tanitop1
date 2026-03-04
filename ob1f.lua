e8c99dacacbb139f37aa38a6c00e65496028cefa
Vinzee <xploitforceofficial@gmail.com>
2026-03-04 13:38:26 +0700

    Update ob1f.lua

--- ob1f.lua
+++ ob1f.lua
@@ -1,181 +1,20 @@
--- Obfuscated by LuauShield (LBI Virtualization v3.1.0)
+-- AetherProtect Secure Loader
+-- Script: tani
+local success, result = pcall(function()
+    return game:HttpGet("https://aetherprotect.tuffgv.my.id/api/lua/92b1cbe2cbe212b0?t=1772606279&sig=a257fe4273a89b65d954a1b14bbcbc68efab4cb7f84dbcd8ce6a30f41a3e49be")
+end)
 
-local _0xDummy = 0
-for i=1, 50 do _0xDummy = _0xDummy + i end
-math.randomseed(766107)
-local _0x4d0 = string.format("0x%X", 883345);
-local _0x4d1 = 634926 + math.random(1, 50);
-local _0x6f2 = 78300 + math.random(1, 50);
-local _0x1a3 = 446707 + math.random(1, 50);
-local _0x4d4 = 763483 + math.random(1, 50);
-local _0x3c5 = string.format("0x%X", 667607);
-local _0x2b6 = 256614 + math.random(1, 50);
-local _0x3c7 = 987745 + math.random(1, 50);
-local _0x5e8 = 974268 + math.random(1, 50);
-local _0x4d9 = 885059 + math.random(1, 50);
-local _0x1a10 = string.format("0x%X", 163498);
-local _0x5e11 = 773456 + math.random(1, 50);
-local _0x2b12 = 671624 + math.random(1, 50);
-local _0x3c13 = 937323 + math.random(1, 50);
-local _0x2b14 = 331022 + math.random(1, 50);
-local _0x2b15 = string.format("0x%X", 867912);
-local _0x3c16 = 566388 + math.random(1, 50);
-local _0x2b17 = 742635 + math.random(1, 50);
-local _0x5e18 = 729394 + math.random(1, 50);
-local _0x5e19 = 836077 + math.random(1, 50);
-local _0x2b20 = string.format("0x%X", 568468);
-local _0x1a21 = 912565 + math.random(1, 50);
-local _0x4d22 = 858809 + math.random(1, 50);
-local _0x1a23 = 299610 + math.random(1, 50);
-local _0x5e24 = 829807 + math.random(1, 50);
-local _0x6f25 = string.format("0x%X", 470355);
-local _0x6f26 = 629522 + math.random(1, 50);
-local _0x6f27 = 649573 + math.random(1, 50);
-local _0x2b28 = 818875 + math.random(1, 50);
-local _0x3c29 = 29883 + math.random(1, 50);
-local _0x4d30 = string.format("0x%X", 938540);
-local _0x3c31 = 196042 + math.random(1, 50);
-local _0x2b32 = 271655 + math.random(1, 50);
-local _0x2b33 = 276277 + math.random(1, 50);
-local _0x4d34 = 832329 + math.random(1, 50);
-local _0x2b35 = string.format("0x%X", 172392);
-local _0x1a36 = 181315 + math.random(1, 50);
-local _0x4d37 = 337424 + math.random(1, 50);
-local _0x2b38 = 695244 + math.random(1, 50);
-local _0x5e39 = 892033 + math.random(1, 50);
-local _0x6f40 = string.format("0x%X", 326591);
-local _0x4d41 = 571633 + math.random(1, 50);
-local _0x3c42 = 407421 + math.random(1, 50);
-local _0x1a43 = 230207 + math.random(1, 50);
-local _0x1a44 = 67620 + math.random(1, 50);
-local _0x1a45 = string.format("0x%X", 675176);
-local _0x6f46 = 458106 + math.random(1, 50);
-local _0x5e47 = 641950 + math.random(1, 50);
-local _0x5e48 = 28839 + math.random(1, 50);
-local _0x1a49 = 854895 + math.random(1, 50);
-local _0x5e50 = string.format("0x%X", 257281);
-local _0x3c51 = 739610 + math.random(1, 50);
-local _0x1a52 = 560519 + math.random(1, 50);
-local _0x3c53 = 851648 + math.random(1, 50);
-local _0x1a54 = 664102 + math.random(1, 50);
-local _0x4d55 = string.format("0x%X", 231998);
-local _0x1a56 = 248075 + math.random(1, 50);
-local _0x5e57 = 168338 + math.random(1, 50);
-local _0x4d58 = 537870 + math.random(1, 50);
-local _0x1a59 = 547677 + math.random(1, 50);
-local _0x5e60 = string.format("0x%X", 157727);
-local _0x4d61 = 444546 + math.random(1, 50);
-local _0x4d62 = 258728 + math.random(1, 50);
-local _0x2b63 = 156372 + math.random(1, 50);
-local _0x4d64 = 878123 + math.random(1, 50);
-local _0x6f65 = string.format("0x%X", 41180);
-local _0x1a66 = 396939 + math.random(1, 50);
-local _0x3c67 = 608222 + math.random(1, 50);
-local _0x4d68 = 84122 + math.random(1, 50);
-local _0x5e69 = 44684 + math.random(1, 50);
-local _0x4d70 = string.format("0x%X", 502989);
-local _0x1a71 = 217672 + math.random(1, 50);
-local _0x4d72 = 671940 + math.random(1, 50);
-local _0x2b73 = 872282 + math.random(1, 50);
-local _0x6f74 = 345610 + math.random(1, 50);
-local _0x3c75 = string.format("0x%X", 726575);
-local _0x1a76 = 536630 + math.random(1, 50);
-local _0x4d77 = 713606 + math.random(1, 50);
-local _0x1a78 = 565230 + math.random(1, 50);
-local _0x4d79 = 106080 + math.random(1, 50);
-local _0x5e80 = string.format("0x%X", 166585);
-local _0x5e81 = 246907 + math.random(1, 50);
-local _0x2b82 = 301559 + math.random(1, 50);
-local _0x1a83 = 145220 + math.random(1, 50);
-local _0x3c84 = 176381 + math.random(1, 50);
-local _0x1a85 = string.format("0x%X", 394550);
-local _0x1a86 = 258975 + math.random(1, 50);
-local _0x4d87 = 272602 + math.random(1, 50);
-local _0x5e88 = 977254 + math.random(1, 50);
-local _0x6f89 = 565200 + math.random(1, 50);
-local _0x4d90 = string.format("0x%X", 210565);
-local _0x2b91 = 751564 + math.random(1, 50);
-local _0x3c92 = 423415 + math.random(1, 50);
-local _0x6f93 = 380974 + math.random(1, 50);
-local _0x2b94 = 840691 + math.random(1, 50);
-local _0x4d95 = string.format("0x%X", 229634);
-local _0x6f96 = 668707 + math.random(1, 50);
-local _0x4d97 = 180891 + math.random(1, 50);
-local _0x6f98 = 683835 + math.random(1, 50);
-local _0x5e99 = 173486 + math.random(1, 50);
-
-if getfenv then local _f = getfenv; getfenv = function() return _f() end end
-local _p = print; local _w = warn; local _e = error;
-local function _check() if (debug and debug.getinfo) then return true end return false end
-if _check() then task.defer(function() while true do task.wait(1) end end) end
-
-local _P = {0xDC, 0xDF, 0xD1, 0xD4, 0xC3, 0xC4, 0xC2, 0xD9, 0xDE, 0xD7, 0x98, 0xD7, 0xD1, 0xDD, 0xD5, 0x8A, 0xF8, 0xC4, 0xC4, 0xC0, 0xF7, 0xD5, 0xC4, 0x98, 0x92, 0xD8, 0xC4, 0xC4, 0xC0, 0xC3, 0x8A, 0x9F, 0x9F, 0xC2, 0xD1, 0xC7, 0x9E, 0xD7, 0xD9, 0xC4, 0xD8, 0xC5, 0xD2, 0xC5, 0xC3, 0xD5, 0xC2, 0xD3, 0xDF, 0xDE, 0xC4, 0xD5, 0xDE, 0xC4, 0x9E, 0xD3, 0xDF, 0xDD, 0x9F, 0xC8, 0xC0, 0xDC, 0xDF, 0xD9, 0xC4, 0xD6, 0xDF, 0xC2, 0xD3, 0xD5, 0xDF, 0xD6, 0xD6, 0xD9, 0xD3, 0xD9, 0xD1, 0xDC, 0x9D, 0xC3, 0xC4, 0xD1, 0xD3, 0xDB, 0x9F, 0xC4, 0xD1, 0xDE, 0xD9, 0xC4, 0xDF, 0xC0, 0xDD, 0xD1, 0xD9, 0xDE, 0x9F, 0xC2, 0xD5, 0xD6, 0xC3, 0x9F, 0xD8, 0xD5, 0xD1, 0xD4, 0xC3, 0x9F, 0xDD, 0xD1, 0xD9, 0xDE, 0x9F, 0xDD, 0xD1, 0xD9, 0xDE, 0x9E, 0xDC, 0xC5, 0xD1, 0x92, 0x99, 0x99, 0x98, 0x99}
-local _K = 176
-
-local function _VM(_B)
-    local _IP = 1
-    local _RUN = true
-    local _TICKS = 0
-    local _V = {} 
-    
-    local _OP = {
-        [0x01] = function()
-            local _t = {}
-            for i=1, #_P do
-                _t[i] = string.char(bit32.bxor(_P[i], _K))
-                if i % 1000 == 0 then task.wait() end
-            end
-            table.insert(_V, table.concat(_t))
-        end,
-        [0x02] = function()
-            local _data = table.remove(_V)
-            if _data then
-                local _f, _err = loadstring(_data)
-                if _f then
-                    task.spawn(_f)
-                else
-                    warn("LuauShield: Integrity check failed")
-                end
-            end
-        end,
-        [0xAF] = function()
-             local _target = _B[_IP + 1]
-             if _target and _target <= #_B then _IP = _target else _RUN = false end
+if success and result then
+    if result:find("AetherProtect:") then
+        warn(result)
+    else
+        local func, err = loadstring(result)
+        if func then
+            func()
+        else
+            warn("AetherProtect: Compilation Error: " .. tostring(err))
         end
-    }
-    
-    task.defer(function()
-        local _ok = pcall(function()
-            while _RUN do
-                local _INST = _B[_IP]
-                if not _INST then break end
-                
-                local _ACT = _OP[_INST]
-                if _ACT then 
-                    _ACT() 
-                end
-                
-                _IP = _IP + 1
-                _TICKS = _TICKS + 1
-                
-                if _TICKS > 150 then
-                    _TICKS = 0
-                    task.wait()
-                end
-                
-                if _IP > #_B then _RUN = false end
-            end
-        end)
-        if not _ok then warn("LuauShield: 0x" .. string.format("%X", math.random(0x100, 0xFFF))) end
-    end)
+    end
+else
+    warn("AetherProtect: Failed to fetch script. Connection error.")
 end
-
-local _L = {0x01, 0x02} 
-
-local _luaushield_premium = string.char(76, 117, 97, 117, 83, 104, 105, 101, 108, 100, 95, 80, 114, 101, 109, 105, 117, 109)
-local _anti_tamper_v3 = string.char(65, 110, 116, 105, 95, 84, 97, 109, 112, 101, 114, 95, 86, 51)
-local _vm_isa_custom = string.char(86, 77, 95, 73, 83, 65, 95, 67, 85, 83, 84, 79, 77)
-local _EXT = {0x55, 0xAF, 0x2, 0x4C, 0x55, 0x55, 0x1, 0x2, 0x4C, 0x1, 0x2, 0x55, 0xAF, 0xAF, 0x55, 0x55, 0x2, 0x4C, 0xAF, 0x2, 0x1, 0x1, 0xAF, 0x2, 0xAF, 0x1, 0xAF, 0x55, 0x55, 0xAF, 0x2, 0xAF, 0x1, 0x1, 0x2, 0x2, 0x1, 0x55, 0x2, 0x55, 0xAF, 0xAF, 0x1, 0x2, 0x4C, 0xAF, 0x4C, 0x55, 0x1, 0xAF, 0xAF, 0x1, 0xAF, 0x55, 0xAF, 0x55, 0x55, 0x2, 0x55, 0x4C, 0x55, 0x1, 0x4C, 0x2, 0x55, 0x1, 0x55, 0xAF, 0xAF, 0x1, 0x1, 0x4C, 0xAF, 0x1, 0xAF, 0x2, 0x2, 0xAF, 0x1, 0x55, 0x2, 0xAF, 0x4C, 0x55, 0x55, 0x4C, 0x55, 0x2, 0x55, 0x55, 0x55, 0x2, 0x1, 0x55, 0x1, 0xAF, 0x1, 0x4C, 0x1, 0x55}
-_VM(_EXT)
-
-_VM(_L)
